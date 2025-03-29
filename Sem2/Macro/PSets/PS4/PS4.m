17c3fbbde685e6cac16f60be062f6082176472ab
HistoricalFool <tatem8499@gmail.com>
2025-03-28 20:02:08 -0400

    lalala

--- Sem2/Macro/PSets/PS4/PS4.m
+++ Sem2/Macro/PSets/PS4/PS4.m
@@ -24,52 +24,27 @@ m = 3;
 
 y_state = zeros(n,1);
 for i = 1:n
-  y_states(i) = uncon_mean + sqrt(2)*uncon_sd*x(i);
+  y_state(i) = uncon_mean + sqrt(2)*sigma*x(i);
 end
 
 % Transition Probability
 P = zeros(n, n);
 
 for i = 1:n
-  mean_next = mu + rho*y_states(i);
+  mean_next = mu + rho*y_state(i);
   for j = 1:n
-    P(i,j) = w(j) * normpdf(y_states(j), mean_next, sigma) / ...
-             normpdf(y_states(j), uncon_mean, uncon_sd);
+    P(i,j) = w(j)/sqrt(pi)*exp(-(0.5)*((y_state(j) - rho*y_state(i))/sigma)^2)/exp(-x(j)^2);
   end
-  P(i,:) = P(i,:) / sum(P(i,:));
+  dens = sum(P(i,:));
+  P(i,:) = P(i,:)/dens;
 end
 
 %Print results
 disp('Discretized State Space for y: ');
-disp(y_states);
+disp(y_state);
 
 disp('Transition Matrix: ');
 disp(P);
 
 %% Problem 2
-
-%Simulation Definition
-n_obs = 1000000;
-dis = 1000;
-total_sim = n_obs + dis;
-
-%Start at Midpoint
-state_sim = zeros(total_sim,1);
-state_sim(1) = ceil(n/2);
-
-%Seed for reproducibility
-rng(19);
-u = rand(total_sim, 1);
-
-%Simulate
-for t = 1:total_sim-1
-  cum_prob = cumsum(P(state_sim(t),:)');
-  state_sim(t+1) = find(u(t) <= cum_prob, 1);
-end
-
-y_sim = zeros(n_obs, 1);
-for t = 1:n_obs
-  y_sim(t) = y_states(state_sim(t+dis));
-end
-
 
