6645a2ab987006e062eae9301be5e9376fd142ca
HistoricalFool <tatem8499@gmail.com>
2025-03-30 14:33:27 -0400

    falskfj

--- Sem2/Macro/PSets/PS4/PS4.m
+++ Sem2/Macro/PSets/PS4/PS4.m
@@ -48,3 +48,37 @@ disp(P);
 
 %% Problem 2
 
+n = 1000000;
+dis = 1000;
+
+y_sim = zeros(n, 1);
+
+init = randi(length(y_state));
+y_sim(1) = y_state(init);
+
+current_state = init;
+
+for t = 2:n
+  cum_prob = cumsum(P(current_state, :));
+  r = rand();
+  next_state = find(r <= cum_prob, 1, 'first');
+  y_sim(t) = y_state(next_state);
+  current_state = next_state;
+end
+
+y_sim_final = y_sim(dis+1:end);
+
+y_t = y_sim_final(1:end-1);
+y_tp1 = y_sim_final(2:end);
+
+y_mean = mean(y_sim_final);
+y_demean = y_t - y_mean;
+y_tp1_demean = y_tp1 - y_mean;
+
+rho_hat = (y_demean' * y_tp1_demean)/(y_demean'*y_demean);
+
+e_hat = y_tp1 - (mu + rho_hat*y_t);
+sigma_hat = sqrt(mean(e_hat.^2));
+
+fprintf('Estimated autocorrelation coefficient (rho_hat): %.6f\n', rho_hat);
+fprintf('Estimated standard deviation (sigma_hat): %.6f\n', sigma_hat);
