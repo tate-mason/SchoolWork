cf43f620f8345bae4b825f9b157df60614c44938
HistoricalFool <tatem8499@gmail.com>
2025-04-20 16:33:38 -0400

    macro ps6 start

---
+++ Sem2/Macro/PSets/PS6/PS6.m
@@ -0,0 +1,15 @@
+%% Tate Mason - PS6
+
+%% Setting Parameters
+T = 65;
+R = 40;
+n = 0.011;
+k1 = 0;
+tau = 0.11;
+mu = 0.5;
+sigma = 3;
+beta = 0.96;
+alpha = 0.36;
+delta = 0.06;
+rho = 0.9;
+sigma_sq =  0.03;
